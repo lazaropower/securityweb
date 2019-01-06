84ddd58921b4730088de2f2a2d8458d3aaa4cc79
Ángel del Castillo <angel7delcastillo@gmail.com>
2019-01-06 17:45:10 +0100

    new database file

--- database/securityweb.sql
+++ database/securityweb.sql
@@ -1,39 +1,49 @@
--- phpMyAdmin SQL Dump
--- version 4.8.4
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 02-01-2019 a las 19:07:34
--- Versión del servidor: 10.1.37-MariaDB
--- Versión de PHP: 7.3.0
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+# ************************************************************
+# Sequel Pro SQL dump
+# Versión 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.5.5-10.3.11-MariaDB)
+# Base de datos: securityweb
+# Tiempo de Generación: 2019-01-06 16:44:02 +0000
+# ************************************************************
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Base de datos: `securityweb`
---
 
--- --------------------------------------------------------
+# Volcado de tabla routers
+# ------------------------------------------------------------
 
---
--- Estructura de tabla para la tabla `test`
---
+DROP TABLE IF EXISTS `routers`;
 
-CREATE TABLE `test` (
+CREATE TABLE `routers` (
   `id` int(2) NOT NULL,
-  `name` varchar(32) NOT NULL
+  `brand` varchar(32) NOT NULL DEFAULT '',
+  `model` varchar(64) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `numAntennas` int(11) DEFAULT NULL,
+  `numLANPorts` int(11) DEFAULT NULL,
+  `MAC_ACL` tinyint(1) DEFAULT NULL,
+  `MU_MIMO` tinyint(1) DEFAULT NULL,
+  `radius` tinyint(1) DEFAULT NULL,
+  `band` enum('single','dual','tri') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-COMMIT;
 
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
